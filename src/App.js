b601b9548405268c1c080c1854b0fcc48ce6e1e8
B-Warissara <s6203051613247@email.kmutnb.ac.th>
2022-08-26 10:48:36 +0700

    add func-components.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Header,Content,Footer} from './func-components'
+
+function App() {
+  return (
+    [<Header />, <Content/>, <Footer/>]
+  )
+   
+}
+
+export default App;
